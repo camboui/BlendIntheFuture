a9a9998cb5641b4cccfa0749b2f42535278bba49
camboui <mathias.branger@gmail.com>
2016-10-29 01:06:16 -0400

    initial commit

---
+++ Assets/Scripts/CheckNewControllers.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CheckNewControllers : MonoBehaviour {
+
+	public GameObject prefab_PlayerChoice;
+
+	private GameObject canvas;
+	private int nb_controller;
+
+	// Use this for initialization
+	void Start () {
+		nb_controller = 0;
+		canvas = GameObject.Find ("Canvas");
+
+	}
+
+	void Update()
+	{
+		string[] inputs = Input.GetJoystickNames ();
+		if(inputs.Length !=nb_controller){
+			nb_controller++;
+			foreach (string p in Input.GetJoystickNames()) {
+				Debug.Log ("Input change : " + p);
+				GameObject temp = Instantiate (prefab_PlayerChoice) as GameObject;
+				temp.transform.SetParent (canvas.transform);
+			}
+		}
+	}
+
+	
+
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour{
+
+	public void loadScene(string sceneName){
+
+		if (SceneManager.GetSceneByName (sceneName)!=null) {
+			Debug.Log ("Loading : " + sceneName);
+			SceneManager.LoadScene (sceneName);
+		} else {
+			Debug.Log ("Can't load unexisting scene : '" + sceneName + "'");
+		}
+	}
+}
