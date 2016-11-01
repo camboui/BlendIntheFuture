65e08870e369da8697aece967d99bdbe9ea38e0c
camboui <mathias.branger@gmail.com>
2016-10-31 21:48:38 -0400

    add settings for controller manager

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PlayerController : MonoBehaviour
+
+{
+	public int relatedController;
+	private Vector3 movementVector;
+	private float movementSpeed = 0.1f;
+
+
+	void Start()
+	{
+		 
+	}
+
+
+
+	void Update()
+	{
+		movementVector.x = Input.GetAxis("X_"+relatedController) * movementSpeed;
+		movementVector.y = Input.GetAxis("Y_"+relatedController) * movementSpeed;
+
+		if(Input.GetButtonDown ("A_"+relatedController))
+			print ("P"+relatedController+" : A");
+		if(Input.GetButtonDown ("B_"+relatedController))
+			print ("P"+relatedController+" : B");
+
+		if(Input.GetButtonDown ("Start_"+relatedController))
+			print ("P"+relatedController+" : Start");
+
+		transform.position += movementVector;
+	}
+
+}
