f04506832801b32f526ac3d60b6726c5c4f678ea
camboui <mathias.branger@gmail.com>
2016-11-05 14:02:18 -0400

    remove custom controllers, use unity buttons and events

--- Assets/Resources/Scripts/PauseMenuController.cs
+++ Assets/Resources/Scripts/PauseMenuController.cs
@@ -6,11 +6,6 @@ using UnityEngine.SceneManagement;
 
 public class PauseMenuController : MonoBehaviour {
 
-	public Text[] selectables;
-	public int currentSelection; //current text selected
-	public bool[] changedRecently; //if player pushed axis button recently, prevent from spamming
-	private List<XboxInput> xboxInputs;
-
 	private List<MonoBehaviour> disabledScripts;
 	private float previousTimeScale;
 
@@ -29,25 +24,8 @@ public class PauseMenuController : MonoBehaviour {
 			}
 		}
 	}
-
-
-	void Start () {	
-		xboxInputs = new List<XboxInput> ();
-
-		//Get all usable texts
-		selectables = transform.GetComponentsInChildren <Text> ();
-		changedRecently = new bool[4];
-		currentSelection = 0;
-
-		//Initialise 
-		for (int i = 0; i < 4; i++) {
-			changedRecently [i] = false;
-			xboxInputs.Add(new XboxInput(i+1));
-		}
-		selectables [currentSelection].color = Color.red;
-	}
-
-	void resumeGame(){
+		
+	public void resumeGame(){
 		SceneManager.UnloadScene ("PauseMenu");
 		//set back timeScale
 		Time.timeScale = previousTimeScale;
@@ -58,57 +36,11 @@ public class PauseMenuController : MonoBehaviour {
 
 	}
 
-	void Update(){
-		//Foreach controller, handle actions
-		for (int j = 0; j < 4; j++) {
-
-			float joyStickY = xboxInputs [j].getYaxis ();
-
-			//if joystick is back approximately in the middle of its axis, it can be used again
-			if (joyStickY < 0.5f && joyStickY > -0.5f)
-				changedRecently [j] = false;
-
-			//Up direction : change selected text
-			if (joyStickY == 1 && !changedRecently [j]) {
-				selectables [currentSelection].color = Color.white;
-
-				currentSelection--;
-				if (currentSelection == -1)
-					currentSelection = selectables.Length - 1;
-
-				selectables [currentSelection].color = Color.red;
-
-				changedRecently [j] = true;
-			}
-			//Down direction : change selected text
-			else if (joyStickY == -1 && !changedRecently [j]) {
-
-				selectables [currentSelection].color = Color.white;
-
-				currentSelection++;
-				if (currentSelection == selectables.Length)
-					currentSelection = 0;
-
-				selectables [currentSelection].color = Color.red;
-
-				changedRecently [j] = true;
-			}
-
-			//Handle button actions
-			if (Input.GetKeyDown (xboxInputs [j].BStart))
-				resumeGame ();
-			else if (Input.GetKeyDown (xboxInputs [j].A)) {
-				if (selectables [currentSelection].text.Equals ("Resume")) {
-					resumeGame ();
-				} else if (selectables [currentSelection].transform.name.Equals ("Help")) {
-					//SceneManager.LoadScene ("Help");
-				} else if (selectables [currentSelection].transform.name.Equals ("Quit")) {
-					Time.timeScale = previousTimeScale;
-					SceneManager.LoadScene ("PlayerSelectionMenu");
-				}
-			}
-
-		}
-
+	public void quit(string sceneName){
+		Time.timeScale = previousTimeScale;
+		SceneManager.LoadScene (sceneName);
 	}
+
+
+
 }
---
+++ Assets/Resources/Scripts/SceneLoader.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+	public void loadScene(string sceneName){
+		SceneManager.LoadScene (sceneName);
+	}
+
+	public void loadScene(int sceneIndex){
+		SceneManager.LoadScene (sceneIndex);
+	}
+
+	public void quit(){
+		Application.Quit();
+	}
+}
+
+
+
