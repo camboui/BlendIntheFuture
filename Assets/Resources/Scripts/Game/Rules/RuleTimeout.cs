ae87f94a658ee6da24125f186d848a9790b0f42f
Matthieu CROUZET <Matthieu CROUZET>
2016-12-04 18:04:09 -0500

    timeout rule

---
+++ Assets/Resources/Scripts/Game/Rules/RuleTimeout.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class RuleTimeout : MonoBehaviour {
+
+	public string nextSceneName;
+
+	// Use this for initialization
+	void Start () {
+		if (nextSceneName == null || nextSceneName.Equals(""))
+			nextSceneName = "EndGame";
+		//Check rule Every 1 second. Let some time for draw 
+		InvokeRepeating("checkEndGame", 1.0f, 1.0f);
+	}
+
+	void checkEndGame()
+	{
+		if (UpdateTimer.time < 0) {
+			foreach (Human current in GameVariables.players) {
+				if (!current.isDead ()) {
+					current.suicide ();
+				}
+			}
+			SceneManager.LoadScene (nextSceneName);
+		}
+	}
+}
--- Assets/Resources/Scripts/Utils/UpdateTimer.cs
+++ Assets/Resources/Scripts/Utils/UpdateTimer.cs
@@ -4,18 +4,22 @@ using UnityEngine.UI;
 
 public class UpdateTimer : MonoBehaviour {
 
+	public int minuteStart;
+	public int secondeStart;
+
 	// Use this for initialization
 	private Text t ;
-	private float time;
+	public static float time;
 	void Start () {
 		t = transform.GetComponent<Text> ();
+		time = minuteStart*60 + secondeStart;
 	}
 
 	// Update is called once per frame
 	void Update () {
-		time += Time.deltaTime;
+		time -= Time.deltaTime;
 
-		float minutes = time / 60;
+		float minutes = Mathf.Floor(time / 60);
 		float seconds = time % 60;
 		t.text = string.Format ("{0:00} : {1:00}", minutes, seconds);
 	}
