57b307b6dee57f4be570b991df4bc4e7281fa5b8
camboui <mathias.branger@gmail.com>
2016-12-04 08:48:04 -0500

    fix bullet nullException

--- Assets/Resources/Scripts/Weapons/BulletSpawner.cs
+++ Assets/Resources/Scripts/Weapons/BulletSpawner.cs
@@ -3,13 +3,12 @@ using System.Collections;
 
 public class BulletSpawner : MonoBehaviour {
 
-	private Transform playerGameObject;
+	public Transform playerParent;
 	private Transform bulletSpawn;
 	public GameObject bulletPrefab;
 	private Human humanOwner;
 
 	void Start () {
-		playerGameObject = transform.parent.parent.parent;
 		bulletSpawn = transform.FindChild ("BulletSpawn").transform;
 		humanOwner = transform.GetComponentInParent<HumanController> ().human;
 	}
@@ -17,6 +16,6 @@ public class BulletSpawner : MonoBehaviour {
 	public void spawnBullet () {
 		GameObject bullet = Instantiate(bulletPrefab) as GameObject;
 		bullet.transform.position = bulletSpawn.position;
-		bullet.GetComponentInChildren<Bullet> ().initialiseBullet (humanOwner, Mathf.Sign(playerGameObject.localScale.x)* 5.0f);
+ 		bullet.GetComponentInChildren<Bullet> ().initialiseBullet (humanOwner, Mathf.Sign(playerParent.localScale.x)* 5.0f);
 	}
 }
