64b0a7820a7ce478cef782d26a8dc78f277a1380
camboui <mathias.branger@gmail.com>
2016-12-03 22:53:18 -0500

    reactivate GO / fix player shooting direction

--- Assets/Resources/Scripts/Weapons/BulletSpawner.cs
+++ Assets/Resources/Scripts/Weapons/BulletSpawner.cs
@@ -2,12 +2,14 @@
 using System.Collections;
 
 public class BulletSpawner : MonoBehaviour {
-	
+
+	private Transform playerGameObject;
 	private Transform bulletSpawn;
 	public GameObject bulletPrefab;
 	private Human humanOwner;
 
 	void Start () {
+		playerGameObject = transform.parent.parent.parent;
 		bulletSpawn = transform.FindChild ("BulletSpawn").transform;
 		humanOwner = transform.GetComponentInParent<HumanController> ().human;
 	}
@@ -15,6 +17,6 @@ public class BulletSpawner : MonoBehaviour {
 	public void spawnBullet () {
 		GameObject bullet = Instantiate(bulletPrefab) as GameObject;
 		bullet.transform.position = bulletSpawn.position;
-		bullet.GetComponentInChildren<Bullet> ().initialiseBullet (humanOwner, Mathf.Sign(transform.localScale.x)* 5.0f);
+		bullet.GetComponentInChildren<Bullet> ().initialiseBullet (humanOwner, Mathf.Sign(playerGameObject.localScale.x)* 5.0f);
 	}
 }
